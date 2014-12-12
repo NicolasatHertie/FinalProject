bc62cabba572ce815a0073763d72592026e54c6a
NicolasatHertie <nicolas.rosemberg@gmail.com>
2014-12-13 00:08:05 +0100

    Delete style.R

--- css/style.R
+++
@@ -1,6 +0,0 @@
-options(rstudio.markdownToHTML = 
-          function(format.css, index.Rmd) {      
-            require(markdown)
-            markdownToHTML(format.css, index.Rmd, stylesheet='format.css')   
-          }
-)
